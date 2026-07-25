542732d570a4655cf7adc205430e097b36acd2dc
Layan-22layan <laaaaaayan.22@gmail.com>
2026-07-26 01:01:34 +0300

    Create home and about-contact page structure

---
+++ about-contact.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'includes/header.php';
+?>
+
+<main>
+
+    <h1>About Us & Contact Us</h1>
+
+    <p>
+        This page provides basic information about our website. 
+        Additionally, students can contact us if they need assistance or have questions answered
+         by providing their contact details.
+    </p>
+
+</main>
+
+<?php
+require_once 'includes/footer.php';
+?>
