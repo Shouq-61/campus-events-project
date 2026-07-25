35eea75ffd2f3f4fda155944b8fbf8750f5454ff
Hala-i6 <halalbakri6@gmail.com>
2026-07-26 01:27:36 +0300

    Create registration page structure

---
+++ registration-form.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'includes/header.php';
+?>
+
+<main>
+
+    <h1>Registration Form Page</h1>
+
+    <p>
+       On this page, a registration form will be designed through which students can register for the event 
+       after selecting it, so that the required information is entered.
+    </p>
+
+</main>
+
+<?php
+require_once 'includes/footer.php';
+?>
---
+++ registration-list.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'includes/header.php';
+?>
+
+<main>
+
+    <h1>Registration List Page</h1>
+
+    <p>
+       On this page, there will be a list of all students who have registered
+        for the various events held on campus.
+    </p>
+
+</main>
+
+<?php
+require_once 'includes/footer.php';
+?>
