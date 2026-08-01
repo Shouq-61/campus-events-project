4154822b6b7c4625d10229701fdd9aaf29da291b
Shouq-61 <shouqalmaghrabi61@gmail.com>
2026-08-01 22:13:59 +0300

    Add campus event data

---
+++ data/events.php
@@ -0,0 +1,68 @@
+<?php
+$events = [
+    [
+        "id" => 1,
+        "title" => "Web Design Workshop",
+        "type" => "Workshop",
+        "date" => "2026-09-01",
+        "time" => "10:00 AM",
+        "location" => "Information Technology Building - 2 ",
+        "description" => "This workshop introduces students to all the design principles used to create user-friendly web pages, including HTML for organizing page structure, CSS for color design, and all formatting.",
+        "image" => "web-design-workshop.png"
+    ],
+
+    [
+        "id" => 2,
+        "title" => "Principles of Financial Management",
+        "type" => "Training Course",
+        "date" => "2026-09-04",
+        "time" => "11:00 AM",
+        "location" => "Business Administration Building - 3",
+        "description" => "This is a training course through which students will be introduced to all the basics of financial management, so that students know everything related to financial planning, controlling expenses, and preparing the budget.",
+        "image" => "financial-management-course.png"
+    ],
+
+    [
+        "id" => 3,
+        "title" => "Programming Challenge",
+        "type" => "Competition",
+        "date" => "2026-09-10",
+        "time" => "9:00 AM",
+        "location" => "Computer Science Building - 1",
+        "description" => "The university student coding competition provides an opportunity for students to test their programming skills and problem-solving abilities within a specific timeframe.",
+        "image" => "programming-challenge.png"
+    ],
+
+    [
+        "id" => 4,
+        "title" => "Digital Content Creation Workshop",
+        "type" => "Workshop",
+        "date" => "2026-09-15",
+        "time" => "12:00 PM",
+        "location" => "Media and Communication Building - 1",
+        "description" => "It is a workshop specifically for university students, through which students are helped to learn the basics and principles of creating digital content in a correct and effective way.",
+        "image" => "digital-media-workshop.png"
+    ],
+
+    [
+        "id" => 5,
+        "title" => "Introduction to Data Analysis",
+        "type" => "Training Course",
+        "date" => "2026-09-19",
+        "time" => "10:30 AM",
+        "location" => "Data Science Building - 4",
+        "description" => "This training course provides university students with the fundamentals of data analysis, so that students learn how to collect data, organize information, as well as interpret and present the results.",
+        "image" => "data-analysis-course.png"
+    ],
+
+    [
+        "id" => 6,
+        "title" => "Cybersecurity Competition",
+        "type" => "Competition",
+        "date" => "2026-09-25",
+        "time" => "11:30 AM",
+        "location" => "Cybersecurity Building - 5",
+        "description" => "In this competition, students will have the opportunity to apply the fundamentals of cybersecurity through various scenarios, and then they will be evaluated based on best practices and problem-solving methods.",
+        "image" => "cybersecurity-competition.png"
+    ]
+];
